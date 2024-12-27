eaf5ddf1523e682374b3ab023192ed22a8f95ce0
황찬호 <cksgh0316@gmail.com>
2024-12-27 11:28:14 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.c
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.c
@@ -3,17 +3,6 @@
 #include <stdlib.h>
 
 int solution(int n) {
-    int answer = 1;
-    
-    if (n <= 0) {
-		return 0;
-	}
-
-	for (int i = 1; i <= n / 2; i++) {
-		if (n % i == 0) {
-			answer += n / i;
-		}
-	}
-    
+    int answer = 0;
     return answer;
 }
