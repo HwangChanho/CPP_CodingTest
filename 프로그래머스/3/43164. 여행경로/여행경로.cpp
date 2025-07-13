36ba81724082f189805462c258bd515b18f46b6a
황찬호 <cksgh0316@gmail.com>
2025-07-13 20:36:01 +0900

    [level 3] Title: 여행경로, Time: 0.06 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+vector<string> solution(vector<vector<string>> tickets) {
+    vector<string> answer;
+    map<string, vector<string>> routes;
+    
+    for (const auto& ticket : tickets) 
+    {
+        routes[ticket[0]].push_back(ticket[1]);
+    }
+    
+    for (auto& pair : routes) 
+    {
+        sort(pair.second.rbegin(), pair.second.rend());
+    }
+    
+    stack<string> s;
+    s.push("ICN");
+    
+    while (!s.empty()) 
+    {
+        string current = s.top();
+
+        if (routes[current].empty()) 
+        {
+            answer.push_back(current);
+            s.pop();
+        } 
+        else 
+        {
+            string next = routes[current].back();
+            routes[current].pop_back();   
+            s.push(next);                 
+        }
+    }
+
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
