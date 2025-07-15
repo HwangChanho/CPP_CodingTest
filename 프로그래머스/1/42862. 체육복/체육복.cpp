9f504e0f48cf3bc60683203d2ab2cf85141a6240
황찬호 <cksgh0316@gmail.com>
2025-07-15 17:08:35 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -2,38 +2,72 @@
 #include <vector>
 #include <algorithm>
 
+#include <map>
+
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
-    sort(lost.begin(), lost.end());
-    sort(reserve.begin(), reserve.end());
+    map<int, vector<int>> cover;
+    int alive = 0;
     
-    vector<int> realLost; 
+    vector<int> to_remove_lost, to_remove_reserve;
     
-    for (auto it = lost.begin(); it != lost.end();) {
-        if (find(reserve.begin(), reserve.end(), *it) != reserve.end()) {
-            reserve.erase(find(reserve.begin(), reserve.end(), *it));
-            it = lost.erase(it);
-        } else {
-            realLost.push_back(*it);
-            ++it;
-        }
-    }
-    
-    for (auto it = realLost.begin(); it != realLost.end();) {
-        bool found = false;
-        for (auto resIt = reserve.begin(); resIt != reserve.end(); ++resIt) {
-            if (*resIt == *it - 1 || *resIt == *it + 1) {
-                reserve.erase(resIt);
-                it = realLost.erase(it);
-                found = true;
+    for (int loose : lost) 
+    {
+        for (int me : reserve) 
+        {
+            if (loose == me) 
+            {
+                to_remove_lost.push_back(loose);
+                to_remove_reserve.push_back(me);
                 break;
             }
         }
-        if (!found) {
-            ++it;
+    }
+    
+    for (int val : to_remove_lost) 
+    {
+        lost.erase(remove(lost.begin(), lost.end(), val), lost.end());
+    }
+    for (int val : to_remove_reserve) 
+    {
+        reserve.erase(remove(reserve.begin(), reserve.end(), val), reserve.end());
+    }
+    
+    for (int avail : reserve)
+    {
+        if (avail - 1 > 0)
+        {
+            cover[avail].push_back(avail - 1);
+        }
+        if (avail + 1 <= n)
+        {
+            cover[avail].push_back(avail + 1);
         }
     }
     
-    return n - realLost.size();
+    sort(lost.begin(), lost.end());
+    
+    for (int loose : lost)
+    {
+        bool found = false;
+        
+        if (cover.count(loose - 1) && !cover[loose - 1].empty()) {
+            alive++;
+            cover[loose - 1].clear();
+            found = true;
+        }
+        else if (cover.count(loose + 1) && !cover[loose + 1].empty()) {
+            alive++;
+            cover[loose + 1].clear();
+            found = true;
+        }
+    }
+    
+    int left = lost.size() - alive;
+    
+    if (left > 0)
+        n -= left;
+    
+    return n;
 }
