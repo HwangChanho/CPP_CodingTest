68b161a6a066945c28d2a2bf5a6e3e529078336e
황찬호 <cksgh0316@gmail.com>
2025-08-07 14:17:58 +0900

    [Gold V] Title: 집합의 표현, Time: 40 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h> // gcc 에만 있음 헤더 묶음
+
+using namespace std;
+
+vector<int> parent;
+int n, m;
+
+int Find(int x)
+{
+    if (parent[x] == x) return x;
+    return parent[x] = Find(parent[x]);
+}
+
+void Union(int x, int y)
+{
+    x = Find(x);
+    y = Find(y);
+
+    if (x != y)
+    {
+        if (x < y)
+            parent[y] = x;
+        else
+            parent[x] = y;
+    }
+}
+
+int main()
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    cin >> n >> m;
+    for (int i = 0; i < n + 1; i++)
+    {
+        parent.push_back(i);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int op, a, b;
+        cin >> op >> a >> b;
+
+        if (op == 0) // Union
+        {
+            Union(a, b);
+        }
+        else // 확인
+        {
+            if (Find(a) == Find(b)) cout << "YES\n";
+            else cout << "NO\n";
+        }
+    }
+}
